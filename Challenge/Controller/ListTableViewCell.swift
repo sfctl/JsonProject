e78cb7dd05f425359b926eaa89412982fae85a23
sfctl <catalbasifa@gmail.com>
2020-02-23 02:29:22 +0300

    first commit

---
+++ Challenge/Controller/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  Challenge
+//
+//  Created by Yasemin YEL on 23.02.2020.
+//  Copyright © 2020 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Challenge/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Challenge
+//
+//  Created by Yasemin YEL on 21.02.2020.
+//  Copyright © 2020 Sifa. All rights reserved.
+//
+
+import Foundation
